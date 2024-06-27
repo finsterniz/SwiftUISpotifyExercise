7eea17e09e77bcf5ded059797d946639bd8538c0
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2024-06-27 12:52:02 +0200

    Initial Commit

---
+++ swftUIPractice1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swftUIPractice1
+//
+//  Created by a on 27.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swftUIPractice1/swftUIPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  swftUIPractice1App.swift
+//  swftUIPractice1
+//
+//  Created by a on 27.06.24.
+//
+
+import SwiftUI
+
+@main
+struct swftUIPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
