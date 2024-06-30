c6232d29c82f77e57c45020ff139398b2b199176
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2024-06-30 13:22:40 +0200

    done: header of SpotifyPlaylistView

--- swftUIPractice1/Shared/SpotifyPlaylistView.swift
+++ swftUIPractice1/Shared/SpotifyPlaylistView.swift
@@ -6,20 +6,27 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct SpotifyPlaylistView: View {
     
     var product: Product = .mock
     var user: User = .mock
     @State var products: [Product] = []
+    @State var showHeader: Bool = true
+//    @State var offset: CGFloat = 0
+//    @State var showTitle: Bool = false
     
     var body: some View {
-        ZStack{
+        ZStack(){
             Color.black.ignoresSafeArea()
             
             ScrollView{
                 LazyVStack(spacing: 12){
                     PlaylistHeaderCell(title: product.title, subTitle: product.brand ?? "", imageName: product.firstImage, height: 250)
+                        .readingFrame { frame in
+                            showHeader = frame.maxY < 130
+                        }
                     
                     PlaylistDescriptionCell(
                         descriptionText: product.description,
@@ -47,6 +54,33 @@ struct SpotifyPlaylistView: View {
                     .padding(.leading, 10)
                 }
             }
+            
+//            Text("current maxY: \(offset)")
+//                .background(.red)
+            
+            ZStack{
+                Text(product.title)
+                    .padding(.vertical, 16)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .background(showHeader ? .spotifyBlack : .clear)
+                    .offset(y: showHeader ? 0 : -50)
+                    .opacity(showHeader ? 1.0 : 0)
+                
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .padding(10)
+                    .background(showHeader ? .spotifyGray.opacity(0.001) : .spotifyGray.opacity(0.7))
+                    .clipShape(Circle())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 16)
+                    .asButton {
+                        
+                    }
+            }
+            .foregroundColor(.spotifyWhite)
+            .frame(maxHeight: .infinity, alignment: .top)
+            .animation(.smooth(duration: 0.4), value: showHeader)
         }
         .task{
             await self.loadData()
@@ -57,7 +91,6 @@ struct SpotifyPlaylistView: View {
     private func loadData() async {
         do{
             self.products = try await DatabaseHelper().getProducts()
-            print("load products: \(products.count)")
         }catch{
             print("Error getting Users or products \(error)")
         }
